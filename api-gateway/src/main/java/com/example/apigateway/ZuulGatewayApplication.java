3bcbce9e4bd3dfb3afbe5d7b2fbf4b9eef5fce57
James Smith <James@Jamess-iMac.fios-router.home>
2018-04-06 05:46:35 -0400

    added annotations to gateway for cors

--- api-gateway/src/main/java/com/example/apigateway/ZuulGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ZuulGatewayApplication.java
@@ -3,6 +3,14 @@ package com.example.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import com.netflix.ribbon.proxy.annotation.Http;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @SpringBootApplication
@@ -11,5 +19,18 @@ public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		config.setAllowedMethods(Arrays.stream(Http.HttpMethod.values()).map(Http.HttpMethod::name).collect(Collectors.toList()));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
